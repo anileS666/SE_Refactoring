a5734b52e5fa8409e64afc9097886c751a76da98
anileS666 <79313463+anileS666@users.noreply.github.com>
2022-05-18 22:50:24 +0200

    Replacing the Conditional Logic

--- src/refactor/Rental.java
+++ src/refactor/Rental.java
@@ -1,4 +1,7 @@
 package refactor;
+
+
+
 public class Rental {
     private Movie movie;
     private int daysRented;
@@ -12,29 +15,13 @@ public class Rental {
     public Movie getMovie() {
         return movie;
     }
-	public double getCharge() {
-	    double result = 0;
-	    switch (getMovie().getPriceCode()) {
-	        case Movie.REGULAR:
-	            result += 2;
-	            if (getDaysRented() > 2)
-	                result += (getDaysRented() - 2) * 1.5;
-	            break;
-	        case Movie.NEW_RELEASE:
-	            result += getDaysRented() * 3;
-	            break;
-	        case Movie.CHILDRENS:
-	            result += 1.5;
-	            if (getDaysRented() > 3)
-	                result += (getDaysRented() - 3) * 1.5;
-	            break;
-	    }
-	    return result;
-	}
+    
+    public double getCharge() {
+    	return movie.getCharge(daysRented);
+    }
+	
+	
 	public int getFrequentRenterPoints() {
-		if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) && this.getDaysRented() > 1) { 
-            return 2;
-		}
-		return 1;
+		return movie.getFrequentRenterPoints(daysRented);
 	}
 }
